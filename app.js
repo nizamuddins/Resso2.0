9e7ab5ec7451ce57f0f26ca49bb7bbb266760f53
nizamuddins <uddinmdnizam61@gmail.com>
2022-09-20 16:02:30 +0530

    project

--- app.js
+++ app.js
@@ -11,18 +11,44 @@ app.set("view engine","ejs")
 app.get('/', (req, res) => res.sendFile(__dirname +"/index.html"));
 app.get('/YourFm',(req,res)=>{setTimeout(()=>{res.render("yourfm")},2000);})
 app.get('/ForYou',(req,res)=>{setTimeout(()=>{res.render("foryou")},2000);})
-app.get('/Browse',(req,res)=>{setTimeout(()=>{res.render("browse")},2000);})
+app.get('/Browse',(req,res)=>{setTimeout(()=>{res.render("browse",{name:"Tu Jaane Na",name2:"tujaanena"})},2000);})
+
+
+
 
 // post
+
+app.post("/browse",(req,res)=>{
+  const request = req.body.songName;
+  let music3 = request.split(" ");
+  let music4 = music3.join("");
+  let music2 = music4.toLowerCase();
+  let src = "songs/"+music2+".mp3"
+   if(request != ""){
+     array = [];
+     array.push(request);     
+     res.render("post",{name:request,name2:src});
+     
+   }else{
+      res.render("post",{name:array[0]});
+   }
+  
+  })
+  
+
+
+
+
 app.post("/song",(req,res)=>{
 const request = req.body.songName;
 let music3 = request.split(" ");
 let music4 = music3.join("");
 let music2 = music4.toLowerCase();
+let src = "songs/"+music2+".mp3"
  if(request != ""){
    array = [];
    array.push(request);     
-   res.render("post",{name:request,name2:music2});
+   res.render("post",{name:request,name2:src});
    
  }else{
     res.render("post",{name:array[0]});
--- public/index2.js
+++ public/index2.js
@@ -1,6 +1,6 @@
 // singernames
 let object = {
-    "jhoom(R&BMIX)": "Jhoom (R&B MIX)-Ali Zafar",
+    "Jhoom(R&BMIX)": "Jhoom (R&B MIX)-Ali Zafar",
     RokeNaRukeNaina: "Arijit-Singh - Badrinath Ki Dulhania",
     tujaanena: 'Atif Aslam - Ajab Prem Ki Gazab Kahani',
     BharDoJholiMeri: "Adnan Sami-Bajrangi Bhaijaan",
@@ -36,7 +36,7 @@ $(function () {
         "Boss (Title Song)",
         "Be Intehaan",
         "Bande Hain Hum",
-        "jhoom (R&B MIX)",
+        "Jhoom (R&B MIX)",
         "Roke Na Ruke Naina",
         "Zara Sa (Lofi Flip)"
     ];
@@ -47,148 +47,111 @@ $(function () {
 
 // browse
 let singer = $("#singer");
-let song = $("#song");
+let song1 = $("#song").text();
 
-$("form").submit((e) => {
-    let inputValue = document
-        .querySelector(".input2")
-        .value
-        e
-        .preventDefault();
-    setTimeout(() => {
-        // changingNames
-        if (inputValue !== "") {
-            let songname = inputValue.split(" ")
-            let joinName = songname.join("")
-            let name_1 = object[joinName];
-            song.text(inputValue)
-            singer.text(name_1);
-            // changingImage
-            let lower = joinName.toLowerCase();
-            let static = $("#imagestatic");
-            static.attr("src", "images/" + lower + ".jpg");
-        }
-
-    }, 2000)
-
-})
+$('#playPause1').addClass('playgif2');
+$('#playPause2').addClass('play1');
+$('#playPause2').addClass('fa-play');
 
 // songsbyyourfm
-var song1 = $("#song").text();
 let songname = song1.split(" ")
 let joinName = songname.join("")
 let name_1 = object[joinName];
 singer.text(name_1);
+
 // changingImage
 let lower = joinName.toLowerCase();
 let static = $("#dynamicimage");
 static.attr("src", "images/" + lower + ".jpg");
 
+// ***** playSongs
+
 let audio1 = document.getElementById("audio1");
+console.log(audio1)
+
 let songs = $("#songs1");
+let start = true;
 songs.click(function () {
-audio1.play()
+    lastImgtext(song1, name_1, lower);
+    $(".width").animate({height: "300px"})
+
+    if (start) {
+        audio1.play();
+        start = false;
+        play1(); 
+        $('.lstPlay').removeClass('fa-play');
+        $('.lstPlay').addClass('fa-pause');
+    } else {
+        audio1.pause();
+        start = true;
+        play2();
+        $('.lstPlay').removeClass('fa-pause');
+        $('.lstPlay').addClass('fa-play');
+    }
+
 })
+function lastImgtext(name1, name2, name3) {
 
-//  addingplayandpause
-
-// $('#playPause1').addClass('playgif2');
-// $('#playPause2').addClass('play1');
-// $('#playPause2').addClass('fa-play');
-
-// //  playingsongs
-
-// let songs = $("#songs1");
-// songs.click(function () {
-//     $('#playPause1').addClass('playgif2');
-//     $('#playPause2').addClass('play1');
-//     $('#playPause2').addClass('fa-pause');
-//     $('#playPause2').removeClass('fa-play');
-
-//     $(".width").animate({height: "300px"})
-
-//     let music1 = song.text()
-//     let music3 = music1.split(" ");
-//     let music4 = music3.join("");
-//     let singer = object[music4]
-//     let music2 = music4.toLowerCase()
-
-//     PlayMusic(music2);
-//     lastImgtext(music1, singer);
-//     console.log(singer)
-
-// })
-
-// function PlayMusic(songName2) {
-//     let audio = new Audio("songs/" + songName2 + ".mp3");
-//     $('.lstPlay').removeClass('fa-play');
-//     $('.lstPlay').addClass('fa-pause');
-//     setTimeout(() => {
-//         audio.play();
-
-//         let id = setInterval(() => {
-//             var range = $('#range');
-//             let currenttime = audio.currentTime
-//             let duration = audio.duration;;
-//             let width = (currenttime / duration) * 100;
-//             range.val(width)
-//             if (range.val() == 100) {
-//                 range.val(0);
-//                 clearInterval(id)
-//                 $('.lstPlay').addClass('fa-play');
-//                 $('.lstPlay').removeClass('fa-pause');
-//             }
-
-//         }, 50);
-
-//     }, 2000)
-//     $('.lstPlay').click((e) => {
-//         if ($('.lstPlay').hasClass('fa-play')) {
-//             $('.lstPlay').removeClass('fa-play');
-//             $('.lstPlay').addClass('fa-pause');
-//             audio.play();
-
-//         } else {
-//             $('.lstPlay').removeClass('fa-pause');
-//             $('.lstPlay').addClass('fa-play');
-//             audio.pause();
-
-//         }
-
-//     })
-
-// }
-
-// $('.lstPlay').click((e) => {
-//     lstPlay();
-// })
-// function lstPlay() {
-//     let music1 = song.text()
-//     let music3 = music1.split(" ");
-//     let music4 = music3.join("");
-//     let music2 = music4.toLowerCase();
-//     let audio4 = new Audio("songs/" + music2 + ".mp3");
-
-//     if ($('.lstPlay').hasClass('fa-play')) {
-//         $('.lstPlay').removeClass('fa-play');
-//         $('.lstPlay').addClass('fa-pause');
-//         audio4.play()
-//     } else {
-//         $('.lstPlay').removeClass('fa-pause');
-//         $('.lstPlay').addClass('fa-play');
-//         audio4.pause()
-
-//     }
-
-// }
-// audio.addEventListener('timeupdate', (e) => {     let currenttime =
-// e.target.currentTime changingEvery()     }) })
-function lastImgtext(images, name) {
-    let music2 = images.toLowerCase()
-    let music3 = music2.split(" ");
-    let music4 = music3.join("");
-    $(".lastimg").attr("src", "images/" + music4 + ".jpg")
-    $('.musicName').text(images);
-    $('.singers').text(name)
+    $(".lastimg").attr("src", "images/" + name3 + ".jpg")
+    $('.musicName').text(name1);
+    $('.singers').text(name2)
 
 }
+// range******
+
+audio1.addEventListener("timeupdate", (e) => {
+    var range = $('#range');
+    let currenttime = e.currentTarget.currentTime
+    let duration = e.currentTarget.duration;
+    let width = (currenttime / duration) * 100;
+    range.val(width);
+    if (range.val() == 100) {
+        $('.lstPlay').addClass('fa-play');
+        $('.lstPlay').removeClass('fa-pause');
+        play2();
+        start = true;
+    }
+})
+
+$('.lstPlay').click((e) => {
+    if ($('.lstPlay').hasClass('fa-play')) {
+        $('.lstPlay').removeClass('fa-play');
+        $('.lstPlay').addClass('fa-pause');
+        play1();
+        audio1.play();
+    } else {
+        $('.lstPlay').removeClass('fa-pause');
+        $('.lstPlay').addClass('fa-play');
+       play2();
+        audio1.pause();
+    }
+})
+function play1(){
+    $('#playPause1').addClass('playgif2');
+    $('#playPause2').addClass('play1');
+    $('#playPause2').addClass('fa-pause');
+    $('#playPause2').removeClass('fa-play');
+}
+function play2(){
+    $('#playPause1').addClass('playgif2');
+    $('#playPause2').addClass('play1');
+    $('#playPause2').addClass('fa-play');
+}
+
+// playingsongs let songs = $("#songs1");     songs.click(function () {
+// let music1 = song.text()         let music3 = music1.split(" ");         let
+// music4 = music3.join("");         let singer = object[music4]         let
+// music2 = music4.toLowerCase()PlayMusic(music2);         lastImgtext(music1,
+// singer);         console.log(singer)     })function PlayMusic(songName2) {
+// let audio = new         Audio("songs/" + songName2 + ".mp3");
+// setTimeout(() => {             audio.play();         }, 50);     }, 2000)
+// $('.lstPlay').click((e) => {     lstPlay(); })function lstPlay() {     let
+// music1 = song.text()     let music3 = music1.split(" ");     let music4 =
+// music3.join("");     let music2 = music4.toLowerCase();     let audio4 = new
+// Audio("songs/" + music2 + ".mp3");     if ($('.lstPlay').hasClass('fa-play'))
+// {         $('.lstPlay').removeClass('fa-play');
+// $('.lstPlay').addClass('fa-pause');         audio4.play()     } else {
+// $('.lstPlay').removeClass('fa-pause');
+// $('.lstPlay').addClass('fa-play');         audio4.pause()     } }
+// audio.addEventListener('timeupdate', (e) => {     let currenttime =
+// e.target.currentTime changingEvery() }) } )
